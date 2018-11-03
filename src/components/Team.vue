a7024a74fc12f91b3c8f2d9d1e4ad21ed78701bb
Kyle McNeese <kyle@Kyles-MBP.domain>
2018-11-03 17:22:27 -0500

    starting fpl team algorithm

---
+++ src/components/Team.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    Here is the best team you could have.
+  </div>
+</template>
+
+<script>
+// import {
+//   getStatistics
+// } from "../services/statistics.js";
+
+export default {
+  name: 'Team',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
