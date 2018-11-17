da61c89f4067b848c7be0fb7f43b0cc65c1a6095
Kyle McNeese <kyle@kyles-mbp.lan>
2018-11-16 22:14:56 -0600

    best possible team chosen, but won't be under salary cap

--- src/components/team.vue
+++ src/components/team.vue
@@ -1,30 +1,84 @@
 <template>
   <div>
     Here is the best team you could have.
-    <div v-for="player in stats">
-      {{ player.first_name }} {{ player.second_name }}
+    <div v-for="player in orderedTeam">
+      {{ player.web_name }}
     </div>
+    <div> {{ costOfTeam }} </div>
   </div>
 </template>
 
 <script>
-// Can't find module!!!! ???????
 import { getStatistics } from "../services/statistics.js";
 
 export default {
   name: 'team',
   data() {
     return {
-      stats: []
+      stats: [],
+      team: [],
+      costOfTeam: 0
+    }
+  },
+  methods: {
+    compare: function(a,b) {
+      if (a.total_points > b.total_points)
+        return -1;
+      if (a.total_points < b.total_points)
+        return 1;
+      return 0;
+    },
+    orderByPosition: function(a,b) {
+      if (a.element_type < b.element_type)
+        return -1;
+      if (a.element_type > b.element_type)
+        return 1;
+      return 0;
+    },
+    makeTeam: function() {
+      var keeperCounter = 0;
+      var defenderCounter = 0;
+      var midfielderCounter = 0;
+      var forwardCounter = 0;
+
+      for (var i = 0; i < this.stats.length; i++) {
+        if (this.team.length == 15) {
+          break;
+        } else if (this.stats[i].element_type == 1 && keeperCounter < 2) {
+          this.team.push(this.stats[i]);
+          keeperCounter += 1;
+        } else if (this.stats[i].element_type == 2 && defenderCounter < 5) {
+          this.team.push(this.stats[i]);
+          defenderCounter += 1;
+        } else if (this.stats[i].element_type == 3 && midfielderCounter < 5) {
+          this.team.push(this.stats[i]);
+          midfielderCounter += 1;
+        } else if (this.stats[i].element_type == 4 && forwardCounter < 3) {
+          this.team.push(this.stats[i]);
+          forwardCounter += 1;
+        }
+      }
+    },
+    getCostOfTeam: function() {
+      for (var i = 0; i < this.team.length; i++) {
+        this.costOfTeam += parseFloat(this.team[i].value_season);
+      }
     }
   },
   created() {
     getStatistics()
       .then(response => {
         if (response.status == 200) {
-          this.stats = response.data.elements;
+          this.stats = response.data.elements.sort(this.compare);
+          this.makeTeam();
+          this.getCostOfTeam();
         }
       });
+  },
+  computed: {
+    orderedTeam: function() {
+      return this.team.sort(this.orderByPosition);
+    }
   }
 }
 </script>
